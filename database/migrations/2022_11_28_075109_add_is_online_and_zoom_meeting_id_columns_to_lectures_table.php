cd06d2735f16321499fa6c61838c8440453a73ff
belyy_vv <belyy_vv@groupbwt.com>
2022-11-28 10:02:43 +0200

    feat: migrations with new zoom columns

---
+++ database/migrations/2022_11_28_075109_add_is_online_and_zoom_meeting_id_columns_to_lectures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lectures', function (Blueprint $table) {
+            $table->boolean('is_online')->default(0)->after('category_id');
+            $table->bigInteger('zoom_meeting_id')->unsigned()->index()->nullable()->after('is_online');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lectures', function (Blueprint $table) {
+            $table->dropColumn('is_online');
+            $table->dropColumn('zoom_meeting_id');
+        });
+    }
+};
